72ea6b11768d62bb73a82684878702ba437f313c
Angel Faryshta Guevara <angeldelcaos@gmail.com>
2017-06-06 16:12:20 -0500

    service fixtures

--- src/fixtures/data/DataType.php
+++ src/fixtures/data/DataType.php
@@ -1,36 +1,36 @@
 <?php
 
-use yii\db\Expression as DbExpression;
+$now = new yii\db\Expression('now()');
 
 return [
     [
         'name' => 'string',
         'cast' => 'stringCast',
         'created_by' => 1,
-        'created_at' => new DbExpression('now()'),
+        'created_at' => $now
     ],
     [
         'name' => 'integer',
         'cast' => 'integerCast',
         'created_by' => 1,
-        'created_at' => new DbExpression('now()'),
+        'created_at' => $now
     ],
     [
         'name' => 'float',
         'cast' => 'floatCast',
         'created_by' => 1,
-        'created_at' => new DbExpression('now()'),
+        'created_at' => $now
     ],
     [
         'name' => 'boolean',
         'cast' => 'booleanCast',
         'created_by' => 1,
-        'created_at' => new DbExpression('now()'),
+        'created_at' => $now
     ],
     [
         'name' => 'file',
         'cast' => 'fileCast',
         'created_by' => 1,
-        'created_at' => new DbExpression('now()'),
+        'created_at' => $now
     ],
 ];
---
+++ src/fixtures/data/FieldRule.php
@@ -0,0 +1,109 @@
+<?php
+
+$now = new yii\db\Expression('now()');
+
+return [
+    [
+        'field_id' => 1,
+        'class' => 'trim',
+        'created_by' => 1,
+        'created_at' => $now
+    ],
+    [
+        'field_id' => 1,
+        'class' => 'required',
+        'created_by' => 1,
+        'created_at' => $now
+    ],
+    [
+        'field_id' => 1,
+        'class' => 'string',
+        'created_by' => 1,
+        'created_at' => $now
+    ],
+    [
+        'field_id' => 2,
+        'class' => 'trim',
+        'created_by' => 1,
+        'created_at' => $now
+    ],
+    [
+        'field_id' => 2,
+        'class' => 'required',
+        'created_by' => 1,
+        'created_at' => $now
+    ],
+    [
+        'field_id' => 2,
+        'class' => 'string',
+        'created_by' => 1,
+        'created_at' => $now
+    ],
+    [
+        'field_id' => 3,
+        'class' => 'trim',
+        'created_by' => 1,
+        'created_at' => $now
+    ],
+    [
+        'field_id' => 3,
+        'class' => 'required',
+        'created_by' => 1,
+        'created_at' => $now
+    ],
+    [
+        'field_id' => 3,
+        'class' => 'date',
+        'created_by' => 1,
+        'created_at' => $now
+    ],
+    [
+        'field_id' => 4,
+        'class' => 'trim',
+        'created_by' => 1,
+        'created_at' => $now
+    ],
+    [
+        'field_id' => 4,
+        'class' => 'required',
+        'created_by' => 1,
+        'created_at' => $now
+    ],
+    [
+        'field_id' => 4,
+        'class' => 'email',
+        'created_by' => 1,
+        'created_at' => $now
+    ],
+    [
+        'field_id' => 5,
+        'class' => 'required',
+        'created_by' => 1,
+        'created_at' => $now
+    ],
+    [
+        'field_id' => 5,
+        'class' => 'integer',
+        'created_by' => 1,
+        'created_at' => $now
+    ],
+    [
+        'field_id' => 6,
+        'class' => 'number',
+        'created_by' => 1,
+        'created_at' => $now
+    ],
+    [
+        'field_id' => 6,
+        'class' => 'integer',
+        'created_by' => 1,
+        'created_at' => $now
+    ],
+    [
+        'data_type_id' => 3,
+        'name' => 'income',
+        'label' => 'Income',
+        'created_by' => 1,
+        'created_at' => $now
+    ],
+];
