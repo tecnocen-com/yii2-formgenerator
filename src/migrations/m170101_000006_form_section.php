4e101f01d8fa6ec3915241dc649ea5b39e493be2
Angel Guevara <angeldelcaos@gmail.com>
2017-11-02 14:00:01 -0600

    FieldSection->label null by default.

--- src/migrations/m170101_000006_form_section.php
+++ src/migrations/m170101_000006_form_section.php
@@ -20,7 +20,8 @@ class m170101_000006_form_section extends tecnocen\rmdb\migrations\CreateEntity
             'position' => $this->integer()->unsigned()->notNull(),
             'form_id' => $this->normalKey(),
             'name' => $this->string(16)->notNull(),
-            'label' => $this->string(64)->notNull(),
+            'label' => $this->string(64->defaultValue(null)
+                ->comment('When not set it will use the label on field.'),
         ];
     }
 
