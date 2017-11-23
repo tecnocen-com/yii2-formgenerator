b46326b3e386cb9b018c6687cb9ea851d0f3e9a8
Angel Faryshta Guevara <angeldelcaos@gmail.com>
2017-11-23 05:24:30 -0600

    test skeleton

---
+++ tests/_app/config/console.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'id' => 'yii2-test-console',
+    'basePath' => dirname(__DIR__),
+    'aliases' => [
+        '@tests' => dirname(dirname(__DIR__)),
+        '@tecnocen/formgenerator' => dirname(dirname(dirname(__DIR__))) . '/src',
+        '@tecnocen/oauth2server' => VENDOR_DIR . '/tecnocen/yii2-oauth2-server/src',
+    ],
+    'components' => [
+        'log' => null,
+        'cache' => null,
+        'db' => require __DIR__ . '/db.php',
+    ],
+    'controllerMap' => [
+        'migrate' => [
+            'class' => yii\console\controllers\MigrateController::class,
+            'migrationPath' => null,
+            'migrationNamespaces' => [],
+        ],
+    ],
+];
---
+++ tests/_app/config/test.php
@@ -0,0 +1,53 @@
+<?php
+
+use tecnocen\formgenerator\roa\modules\Version as FormgeneratorVersion;
+use tecnocen\roa\controllers\ProfileResource;
+use tecnocen\roa\hal\JsonResponseFormatter;
+use yii\web\Response;
+
+return [
+    'id' => 'yii2-formgenerator-tests',
+    'basePath' => dirname(__DIR__),
+    'language' => 'en-US',
+    'aliases' => [
+        '@tests' => dirname(dirname(__DIR__)),
+        '@vendor' => VENDOR_DIR,
+        '@bower' => VENDOR_DIR . '/bower',
+    ],
+    'bootstrap' => ['api'],
+    'modules' => [
+        'api' => [
+            'class' => tecnocen\roa\modules\ApiContainer::class,
+            'identityClass' => app\models\User::class,
+            'versions' => [
+                'f1' => [
+                    'class' => FormgeneratorVersion::class,
+                ],
+            ],
+        ],
+    ],
+    'components' => [
+        'db' => require __DIR__ . '/db.php',
+        'mailer' => [
+            'useFileTransport' => true,
+        ],
+        'user' => ['identityClass' => app\models\User::class],
+        'urlManager' => [
+            'showScriptName' => true,
+            'enablePrettyUrl' => true,
+        ],
+        'request' => [
+            'cookieValidationKey' => 'test',
+            'enableCsrfValidation' => false,
+        ],
+        'response' => [
+            'formatters' => [
+                Response::FORMAT_JSON => [
+                    'class' => JsonResponseFormatter::class,
+                    'prettyPrint' => YII_DEBUG,
+                ],
+            ],
+        ],
+    ],
+    'params' => [],
+];
