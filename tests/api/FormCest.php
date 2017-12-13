1cdd9e4a6499a1f3ce25b32442b87b47190e8ff5
Carlos Llamosas <carlos@tecnocen.com>
2017-12-13 09:19:09 -0600

    FormCest Crud Test Operations

--- tests/api/FormCest.php
+++ tests/api/FormCest.php
@@ -5,6 +5,11 @@ use app\fixtures\FormFixture;
 use Codeception\Example;
 use Codeception\Util\HttpCode;
 
+/**
+ * Cest to form resource.
+ *
+ * @author Carlos (neverabe) Llamosas <carlos@tecnocen.com>
+ */
 class FormCest extends \tecnocen\roa\test\AbstractResourceCest
 {
     protected function authToken(ApiTester $I)
@@ -21,7 +26,9 @@ class FormCest extends \tecnocen\roa\test\AbstractResourceCest
     }
 
     /**
-     * @dataprovider indexData
+     * @param  ApiTester $I
+     * @param  Example $example
+     * @dataprovider indexDataProvider
      * @depends fixtures
      * @before authToken
      */
@@ -31,15 +38,199 @@ class FormCest extends \tecnocen\roa\test\AbstractResourceCest
         $this->internalIndex($I, $example);
     }
 
-    protected function indexData()
+    /**
+     * @return array<string,array> for test `index()`.
+     */
+    protected function indexDataProvider()
     {
         return [
-            [
+            'list' => [
+                'url' => '/form',
                 'httpCode' => HttpCode::OK,
+                'headers' => [
+                    'X-Pagination-Total-Count' => 1,
+                ],
+            ],
+            'not found form' => [
+                'url' => '/form/15',
+                'httpCode' => HttpCode::NOT_FOUND,
+            ],
+            'filter by author' => [
+                'urlParams' => [
+                    'created_by' => 1,
+                ],
+                'httpCode' => HttpCode::OK,
+                'headers' => [
+                    'X-Pagination-Total-Count' => 1,
+                ],
+            ],
+            'filter by name' => [
+                'urlParams' => [
+                    'name' => 'ticipa',
+                ],
+                'httpCode' => HttpCode::OK,
+                'headers' => [
+                    'X-Pagination-Total-Count' => 1,
+                ],
+            ],
+            'rule created_by' => [
+                'urlParams' => [
+                    'created_by' => 'wo',
+                ],
+                'httpCode' => HttpCode::UNPROCESSABLE_ENTITY,
             ],
         ];
     }
 
+    /**
+     * @param  ApiTester $I
+     * @param  Example $example
+     * @dataprovider viewDataProvider
+     * @depends fixtures
+     * @before authToken
+     */
+    public function view(ApiTester $I, Example $example)
+    {
+        $I->wantTo('Retrieve Form single record.');
+        $this->internalView($I, $example);
+    }
+
+    /**
+     * @return array<string,array<string,string>> data for test `view()`.
+     */
+    protected function viewDataProvider()
+    {
+        return [
+            'single record' => [
+                'url' => '/form/1',
+                'httpCode' => HttpCode::OK,
+            ],
+            'not found credit record' => [
+                'url' => '/form/8',
+                'httpCode' => HttpCode::NOT_FOUND,
+            ],
+        ];
+    }
+
+    /**
+     * @param  ApiTester $I
+     * @param  Example $example
+     * @dataprovider createDataProvider
+     * @depends fixtures
+     * @before authToken
+     */
+    public function create(ApiTester $I, Example $example)
+    {
+        $I->wantTo('Create a Form record.');
+        $this->internalCreate($I, $example);
+    }
+
+    /**
+     * @return array<string,array> data for test `create()`.
+     */
+    protected function createDataProvider()
+    {
+        return [
+            'create form 1' => [
+                'urlParams' => [
+                    'name' => 'First Math Test',
+                ],
+                'httpCode' => HttpCode::CREATED,
+            ],
+            'unique' => [
+                'data' => ['name' => 'First Math Test'],
+                'httpCode' => HttpCode::UNPROCESSABLE_ENTITY,
+                'validationErrors' => [
+                    'name' => 'Form name "First Math Test" has already been taken.'
+                ],
+            ],
+            'to short' => [
+                'data' => ['name' => 'fo'],
+                'httpCode' => HttpCode::UNPROCESSABLE_ENTITY,
+                'validationErrors' => [
+                    'name' => 'Form name should contain at least 6 characters.'
+                ],
+            ],
+            'not blank' => [
+                'httpCode' => HttpCode::UNPROCESSABLE_ENTITY,
+                'validationErrors' => [
+                    'name' => 'Form name cannot be blank.'
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @param  ApiTester $I
+     * @param  Example $example
+     * @dataprovider updateDataProvider
+     * @depends fixtures
+     * @before authToken
+     */
+    public function update(ApiTester $I, Example $example)
+    {
+        $I->wantTo('Update a Form record.');
+        $this->internalUpdate($I, $example);
+    }
+
+    /**
+     * @return array[] data for test `update()`.
+     */
+    protected function updateDataProvider()
+    {
+        return [
+            'update form 1' => [
+                'urlParams' => ['id' => '1'],
+                'data' => ['name' => 'Second Math Test'],
+                'httpCode' => HttpCode::OK,
+            ],
+            'to short' => [
+                'urlParams' => ['id' => '1'],
+                'data' => ['name' => 'fo'],
+                'httpCode' => HttpCode::UNPROCESSABLE_ENTITY,
+                'validationErrors' => [
+                    'name' => 'Form name should contain at least 6 characters.'
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @param  ApiTester $I
+     * @param  Example $example
+     * @dataprovider deleteDataProvider
+     * @depends fixtures
+     * @before authToken
+     */
+    public function delete(ApiTester $I, Example $example)
+    {
+        $I->wantTo('Delete a Form record.');
+        $this->internalDelete($I, $example);
+    }
+
+    /**
+     * @return array[] data for test `delete()`.
+     */
+    protected function deleteDataProvider()
+    {
+        return [
+            'delete form 1' => [
+                'urlParams' => ['id' => '1'],
+                'httpCode' => HttpCode::NO_CONTENT,
+            ],
+            'not found' => [
+                'urlParams' => ['id' => '1'],
+                'httpCode' => HttpCode::NOT_FOUND,
+                'validationErrors' => [
+                    'name' => 'The record "1" does not exists.'
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
     protected function recordJsonType()
     {
         return [
@@ -48,6 +239,9 @@ class FormCest extends \tecnocen\roa\test\AbstractResourceCest
         ];
     }
 
+    /**
+     * @inheritdoc
+     */
     protected function getRoutePattern()
     {
         return 'form';
