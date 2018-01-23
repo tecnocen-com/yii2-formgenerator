36b95dff6b7d7e8ba9924924c81c507ff35e35f6
Carlos Llamosas <carlos@tecnocen.com>
2018-01-23 07:58:56 -0600

    Add new solicitude

--- tests/_app/fixtures/data/solicitude.php
+++ tests/_app/fixtures/data/solicitude.php
@@ -9,5 +9,12 @@ return [
         'created_at' => $now,
         'updated_by' => 1,
         'updated_at' => $now,
+    ],    
+    [
+        'form_id' => 1,
+        'created_by' => 1,
+        'created_at' => $now,
+        'updated_by' => 1,
+        'updated_at' => $now,
     ],
 ];
