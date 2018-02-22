85167d5923d6958db7e0b0d309ca1b8c01519131
Angel Guevara <angeldelcaos@gmail.com>
2018-02-22 05:53:19 -0600

    enable debug on live demo

--- tests/_app/config/web.php
+++ tests/_app/config/web.php
@@ -4,5 +4,11 @@ return yii\helpers\ArrayHelper::merge(
     require __DIR__ . '/test.php',
     [
         'id' => 'yii2-formgenerator-demo',
+        'bootstrap' => ['debug'],
+        'modules' => [
+            'debug' => [
+                'class' => yii\debug\Module::class,
+            ],
+        ],
     ]
 );
