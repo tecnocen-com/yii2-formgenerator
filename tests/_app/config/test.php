da5aff9139a94820917469c8eb08846436283bbb
Angel Guevara <angeldelcaos@gmail.com>
2018-02-22 05:39:11 -0600

    Support for live demo.

--- tests/_app/config/test.php
+++ tests/_app/config/test.php
@@ -5,52 +5,47 @@ use tecnocen\roa\controllers\ProfileResource;
 use tecnocen\roa\hal\JsonResponseFormatter;
 use yii\web\Response;
 
-return [
-    'id' => 'yii2-formgenerator-tests',
-    'basePath' => dirname(__DIR__),
-    'language' => 'en-US',
-    'aliases' => [
-        '@tests' => dirname(dirname(__DIR__)),
-        '@vendor' => VENDOR_DIR,
-        '@bower' => VENDOR_DIR . '/bower',
-    ],
-    'bootstrap' => ['api'],
-    'modules' => [
-        'api' => [
-            'class' => tecnocen\roa\modules\ApiContainer::class,
-            'identityClass' => app\models\User::class,
-            'versions' => [
-                'f1' => [
-                    'class' => FormgeneratorVersion::class,
+return yii\helpers\ArrayHelper::merge(
+    require __DIR__ . '/common.php',
+    [
+        'id' => 'yii2-formgenerator-tests',
+        'bootstrap' => ['api'],
+        'modules' => [
+            'api' => [
+                'class' => tecnocen\roa\modules\ApiContainer::class,
+                'identityClass' => app\models\User::class,
+                'versions' => [
+                    'f1' => [
+                        'class' => FormgeneratorVersion::class,
+                    ],
+                ],
+            ],
+            'rmdb' => [
+                'class' => tecnocen\rmdb\Module::class,
+            ],
+        ],
+        'components' => [
+            'mailer' => [
+                'useFileTransport' => true,
+            ],
+            'user' => ['identityClass' => app\models\User::class],
+            'urlManager' => [
+                'showScriptName' => true,
+                'enablePrettyUrl' => true,
+            ],
+            'request' => [
+                'cookieValidationKey' => 'test',
+                'enableCsrfValidation' => false,
+            ],
+            'response' => [
+                'formatters' => [
+                    Response::FORMAT_JSON => [
+                        'class' => JsonResponseFormatter::class,
+                        'prettyPrint' => YII_DEBUG,
+                    ],
                 ],
             ],
         ],
-        'rmdb' => [
-            'class' => tecnocen\rmdb\Module::class,
-        ],
-    ],
-    'components' => [
-        'db' => require __DIR__ . '/db.php',
-        'mailer' => [
-            'useFileTransport' => true,
-        ],
-        'user' => ['identityClass' => app\models\User::class],
-        'urlManager' => [
-            'showScriptName' => true,
-            'enablePrettyUrl' => true,
-        ],
-        'request' => [
-            'cookieValidationKey' => 'test',
-            'enableCsrfValidation' => false,
-        ],
-        'response' => [
-            'formatters' => [
-                Response::FORMAT_JSON => [
-                    'class' => JsonResponseFormatter::class,
-                    'prettyPrint' => YII_DEBUG,
-                ],
-            ],
-        ],
-    ],
-    'params' => [],
-];
+        'params' => [],
+    ]
+);
---
+++ tests/_app/config/web.php
@@ -0,0 +1,8 @@
+<?php
+
+return yii\helpers\ArrayHelper::merge(
+    require __DIR__ . '/test.php',
+    [
+        'id' => 'yii2-formgenerator-demo',
+    ]
+);
