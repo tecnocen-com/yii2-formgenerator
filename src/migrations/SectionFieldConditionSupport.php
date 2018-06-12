353ab4d1703c606d506b5b2649afbfe0a73d2d9b
Carlos Llamosas <carlos@tecnocen.com>
2018-06-12 15:44:14 -0500

    Add foreignkeys restrictions for condition_section_id and condition_field_id

--- src/migrations/SectionFieldConditionSupport.php
+++ src/migrations/SectionFieldConditionSupport.php
@@ -14,19 +14,4 @@ trait SectionFieldConditionSupport
             'condition_effect' => $this->string(32)->defaultValue(null)
         ];
     }
-
-    protected function conditionForeignKeys()
-    {
-        return [
-            'condition_section_field' => [
-                'table' => 'formgenerator_form_section_field',
-                'onDelete' => 'restrict',
-                'onUpdate' => 'restrict',
-                'columns' => [
-                    'condition_section_id' => 'section_id',
-                    'condition_field_id' => 'field_id',
-                ],
-            ],
-        ];
-    }
 }
